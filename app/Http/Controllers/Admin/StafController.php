87b335aef79d55a0fa39f17fd2d418accfd0c7a2
farhan wahyudi <farhanwahyudi875@gmail.com>
2025-06-17 07:09:34 +0700

    Get Staf

--- app/Http/Controllers/Admin/StafController.php
+++ app/Http/Controllers/Admin/StafController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
 class StafController extends Controller
 {
     public function staf(): Response {
-        return response()->view('admin.staf');
+        $users = User::with('roles')->get();
+
+        return response()->view('admin.staf', compact('users'));
     }
 }
--- resources/views/admin/staf.blade.php
+++ resources/views/admin/staf.blade.php
@@ -12,15 +12,18 @@
                     <th class="text-[#6D6D6D] text-start w-60">Status</th>
                     <th class="text-[#6D6D6D] text-start w-32">Action</th>
                 </tr>
+                @foreach($users as $user)
                 <tr class="border-y-2">
                     <td class="font-bold flex items-center gap-3 py-2">
                         <svg class="w-10 stroke-1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                         </svg>
-                        User1
+                        {{ $user->name }}
                     </td>
-                    <td class="text-[#6D6D6D] text-start">user1@gmail.com</td>
-                    <td class="text-[#6D6D6D] text-start">User</td>
+                    <td class="text-[#6D6D6D] text-start">{{ $user->email }}</td>
+                    @foreach($user->roles as $role)
+                    <td class="text-[#6D6D6D] text-start">{{ $role->name }}</td>
+                    @endforeach
                     <td class="text-[#EF0004] font-semibold text-start">Nonactive</td>
                     <td>
                         <form action="" class="flex items-center gap-2">
@@ -28,22 +31,7 @@
                         </form>
                     </td>
                 </tr>
-                <tr class="border-y-2">
-                    <td class="font-bold flex items-center gap-3 py-2">
-                        <svg class="w-10 stroke-1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                        </svg>
-                        User2
-                    </td>
-                    <td class="text-[#6D6D6D] text-start">user2@gmail.com</td>
-                    <td class="text-[#6D6D6D] text-start">User</td>
-                    <td class="text-[#00CE6E] font-semibold text-start">Active</td>
-                    <td>
-                        <form action="" class="flex items-center gap-2">
-                            <input type="submit" class="h-10 w-16 bg-[#00DF77] rounded-lg text-white cursor-pointer" value="Edit">
-                        </form>
-                    </td>
-                </tr>
+                @endforeach
             </thead>
         </table>
     </div>
